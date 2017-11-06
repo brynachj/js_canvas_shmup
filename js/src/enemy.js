4385f47da8d759db15c7341bc1cdc40bf8faf6a7
Brynach Jones <brynachj@gmail.com>
2017-11-06 07:59:03 +0000

    Reorder functions in enemy.js

--- js/src/enemy.js
+++ js/src/enemy.js
@@ -5,6 +5,13 @@ const WIDTH = 34, HEIGHT = 36, SPEED = 3;
 enemy_sprite = new Image();
 enemy_sprite.src = 'images/enemy_sprite.png';
 
+function createEnemy(x1, y1) {
+  return {x:x1, y:y1, w:WIDTH, h:HEIGHT, speed:SPEED, hitBoxColor: '#ff0000',
+        player_detection_box : {x:x1-60, y:y1-60, w:WIDTH+120, h:HEIGHT+120, hitBoxColor: '#ff8c00'},
+        player_aggro_box : {x:x1-80, y:y1-80, w:WIDTH+160, h:HEIGHT+160, hitBoxColor: '#ffff00'}
+      };
+}
+
 function drawEnemies(enemies, ctx) {
   for (var i = 0; i < enemies.length; i++) {
     draw_module.drawSprite(enemy_sprite, enemies[i], ctx);
@@ -13,13 +20,6 @@ function drawEnemies(enemies, ctx) {
       draw_module.drawHitbox(enemies[i].player_aggro_box, ctx);
     }
   }
-}
-
-function createEnemy(x1, y1) {
-  return {x:x1, y:y1, w:WIDTH, h:HEIGHT, speed:SPEED, hitBoxColor: '#ff0000',
-        player_detection_box : {x:x1-60, y:y1-60, w:WIDTH+120, h:HEIGHT+120, hitBoxColor: '#ff8c00'},
-        player_aggro_box : {x:x1-80, y:y1-80, w:WIDTH+160, h:HEIGHT+160, hitBoxColor: '#ffff00'}
-      };
 }
 
 function moveEnemy(enemy, target) {
