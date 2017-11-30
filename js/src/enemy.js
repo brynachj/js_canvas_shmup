be227bbc44e75a6b856bee1813f7aa84af5f6f8b
Brynach Jones <brynach.jones@gmail.com>
2017-11-30 07:54:38 +0000

    Attack hitbox is directly in front of the enemy

--- js/src/enemy.js
+++ js/src/enemy.js
@@ -18,7 +18,7 @@ function createEnemy(x1, y1) {
   return {id: utility_module.newId(enemies), x:x1, y:y1, w:WIDTH, h:HEIGHT, speed:SPEED, hitBoxColor: '#ff0000', health: 100,
         player_detection_box : {x:x1-60, y:y1-60, w:WIDTH+120, h:HEIGHT+120, hitBoxColor: '#ff8c00'},
         player_aggro_box : {x:x1-80, y:y1-80, w:WIDTH+160, h:HEIGHT+160, hitBoxColor: '#ffff00'},
-        player_attack_box: {x:x1-5, y:y1-5, w:WIDTH+10, h:HEIGHT+10, hitBoxColor: '#ff6961'},
+        player_attack_box: {x:x1-5, y:y1-5, w:10, h:HEIGHT+10, hitBoxColor: '#ff6961'},
         aggro : false, attacking: false
       };
 }
