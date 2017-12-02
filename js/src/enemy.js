f2357437574a0b01f3fedcc49c3a74120e1f7a79
Brynach Jones <brynach.jones@gmail.com>
2017-12-02 20:11:33 +0000

    Remove unused parameter from collision detection function.

--- js/src/enemy.js
+++ js/src/enemy.js
@@ -182,7 +182,7 @@ function playerEnemyAttackBoxCollision() {
   enemies.filter(enemy => collisionDetection(player_module.getPlayer(), enemy.player_attack_box)).map(enemy => setAttacking(enemy, true));
 }
 
-function collisionDetection(firstThing, secondThing, callbackList){
+function collisionDetection(firstThing, secondThing){
   return firstThing.x < secondThing.x + secondThing.w && firstThing.x + firstThing.w > secondThing.x &&
   firstThing.y < secondThing.y + secondThing.h && firstThing.h + firstThing.y > secondThing.y;
 }
--- js/src/main.js
+++ js/src/main.js
@@ -74,7 +74,7 @@ var canvas,
       pebble_pickup_module.removeFromPebblePickups(pebble);
     }
 
-    function collisionDetection(firstThing, secondThing, callbackList){
+    function collisionDetection(firstThing, secondThing){
       return firstThing.x < secondThing.x + secondThing.w && firstThing.x + firstThing.w > secondThing.x &&
       firstThing.y < secondThing.y + secondThing.h && firstThing.h + firstThing.y > secondThing.y;
     }
