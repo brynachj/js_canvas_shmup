f01427ba68d25bbb4e55757a61effb61654632ae
Brynach Jones <brynach.jones@gmail.com>
2017-12-05 22:07:43 +0000

    Update enemy code so that the enemy is drawn before their weapon.

--- js/src/enemyDrawer.js
+++ js/src/enemyDrawer.js
@@ -23,7 +23,7 @@ attack_sprite_up.src = 'images/enemy_sword_up.png';
 attack_sprite_down = new Image();
 attack_sprite_down.src = 'images/enemy_sword_down.png';
 
-function drawEnemy(enemy_sprite, enemy) {
+function drawEnemy(enemy) {
   if(enemy.facing === LEFT){draw_module.drawSprite(enemy_sprite_left, enemy, draw_module.ctx);}
   if(enemy.facing === RIGHT){draw_module.drawSprite(enemy_sprite_right, enemy, draw_module.ctx);}
   if(enemy.facing === UP){draw_module.drawSprite(enemy_sprite_up, enemy, draw_module.ctx);}
@@ -32,7 +32,7 @@ function drawEnemy(enemy_sprite, enemy) {
 
 function drawEnemies(enemies) {
     enemies.map(enemy => {
-        drawEnemy(enemy_sprite, enemy)
+        drawEnemy(enemy)
         if (window.drawHitboxes) {
             draw_module.drawHitbox(enemy.player_detection_box, draw_module.ctx);
             draw_module.drawHitbox(enemy.player_aggro_box, draw_module.ctx);
@@ -42,6 +42,7 @@ function drawEnemies(enemies) {
 }
 
 function drawWindUpAttack(enemy) {
+  drawEnemy(enemy);
   if(enemy.facing === LEFT){draw_module.drawSprite(attack_sprite_left, {x: enemy.x, y: enemy.y+enemy.h/2}, draw_module.ctx);}
   if(enemy.facing === RIGHT){draw_module.drawSprite(attack_sprite_right, {x: enemy.x, y: enemy.y+enemy.h/2}, draw_module.ctx);}
   if(enemy.facing === UP){draw_module.drawSprite(attack_sprite_up, {x: enemy.x + enemy.w/2, y: enemy.y}, draw_module.ctx);}
@@ -49,14 +50,20 @@ function drawWindUpAttack(enemy) {
 }
 
 function drawAttacking(enemy) {
+  drawEnemy(enemy);
   if(enemy.facing === LEFT){draw_module.drawSprite(attack_sprite_left, {x: enemy.x-30, y: enemy.y+enemy.h/2}, draw_module.ctx);}
   if(enemy.facing === RIGHT){draw_module.drawSprite(attack_sprite_right, {x: enemy.x+30, y: enemy.y+enemy.h/2}, draw_module.ctx);}
   if(enemy.facing === UP){draw_module.drawSprite(attack_sprite_up, {x: enemy.x + enemy.w/2, y: enemy.y-30}, draw_module.ctx);}
   if(enemy.facing === DOWN){draw_module.drawSprite(attack_sprite_down, {x: enemy.x + enemy.w/2, y: enemy.y+30}, draw_module.ctx);}
 }
 
+function drawWindDownAttack(enemy) {
+  drawEnemy(enemy);
+}
+
 module.exports = {
   drawEnemies : drawEnemies,
   drawWindUpAttack : drawWindUpAttack,
-  drawAttacking : drawAttacking
+  drawAttacking : drawAttacking,
+  drawWindDownAttack : drawWindDownAttack
 }
