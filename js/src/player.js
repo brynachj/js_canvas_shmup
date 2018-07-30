218ccb8685af4c8cda7d0fa22ed6f9235c8a05fa
Brynach Jones <brynach.jones@gmail.com>
2018-07-30 20:51:57 +0100

    Added playerMovement.js which handles updating the player's movement and which direction they face.

--- js/src/player.js
+++ js/src/player.js
@@ -1,6 +1,7 @@
 var enemy_manager = require('./enemyManager.js');
 var collision_detection_module = require('./collisionDetection.js');
 var player_drawer = require('./playerDrawer.js');
+var player_movement = require('./playerMovement.js');
 
 const WIDTH = 20, HEIGHT = 26, SPEED = 10, ATTACK_WIDTH = 15;
 
@@ -84,61 +85,9 @@ function updatePlayer(rightKey, leftKey, upKey, downKey) {
   if(player.state === ATTACKING || player.state === WINDING_DOWN){
     attack();
   }
-  movePlayer(rightKey, leftKey, upKey, downKey);
-  facePlayer(rightKey, leftKey, upKey, downKey);
+  player_movement.movePlayer(player, rightKey, leftKey, upKey, downKey);
+  player_movement.facePlayer(player, rightKey, leftKey, upKey, downKey);
   player_drawer.drawPlayer(player);
-}
-
-function facePlayer(rightKey, leftKey, upKey, downKey) {
-  if(upKey) {
-    player.facing =  UP;
-    player.attack_box = {x:player.x-5, y:player.y-10, w:WIDTH+10, h:ATTACK_WIDTH, hitBoxColor: '#ff6961'};
-  }
-  if(downKey) {
-    player.facing =  DOWN;
-    player.attack_box = {x:player.x-5, y:player.y+player.h, w:WIDTH+10, h:ATTACK_WIDTH, hitBoxColor: '#ff6961'};
-  }
-  if(rightKey) {
-    player.facing =  RIGHT;
-    player.attack_box = {x:player.x+player.w, y:player.y-5, w:ATTACK_WIDTH, h:HEIGHT+10, hitBoxColor: '#ff6961'};
-  }
-  if(leftKey) {
-    player.facing =  LEFT;
-    player.attack_box = {x:player.x-10, y:player.y-5, w:ATTACK_WIDTH, h:HEIGHT+10, hitBoxColor: '#ff6961'};
-  }
-}
-
-function movePlayer(rightKey, leftKey, upKey, downKey) {
-  if(player.state !== ATTACKING){
-    if (rightKey && (player.x + player.w) < 600) {
-      colliding_right = enemy_manager.enemies.filter(e => collision_detection_module.collisionDetection({x:player.x + 5, y:player.y, w: WIDTH, h: HEIGHT}, e));
-      if(colliding_right.length === 0){
-        player.x += 5;
-        player.attack_box.x += 5;
-      }
-    }
-    else if (leftKey && player.x > 0) {
-      colliding_left = enemy_manager.enemies.filter(e => collision_detection_module.collisionDetection({x:player.x - 5, y:player.y, w: WIDTH, h: HEIGHT}, e));
-      if(colliding_left.length === 0){
-        player.x -= 5;
-        player.attack_box.x -= 5;
-      }
-    }
-    if (upKey && player.y > 0) {
-      colliding_up = enemy_manager.enemies.filter(e => collision_detection_module.collisionDetection({x:player.x, y:player.y - 5, w: WIDTH, h: HEIGHT}, e));
-      if(colliding_up.length === 0){
-        player.y -= 5;
-        player.attack_box.y -= 5;
-      }
-    }
-    else if (downKey && (player.y + player.h) < 600) {
-      colliding_down = enemy_manager.enemies.filter(e => collision_detection_module.collisionDetection({x:player.x, y:player.y + 5, w: WIDTH, h: HEIGHT}, e));
-      if(colliding_down.length === 0){
-        player.y += 5;
-        player.attack_box.y += 5;
-      }
-    }
-  }
 }
 
 function attack() {
