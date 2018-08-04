112ad2cf918fa0563e26f25c6484dbfe64c2adfd
Brynach Jones <brynach.jones@gmail.com>
2018-08-04 23:32:30 +0100

    Fix enemyService.test.js calling incorrect functions in tests.

--- js/src/enemyService.test.js
+++ js/src/enemyService.test.js
@@ -9,14 +9,14 @@ jest.mock('./player.js')
 jest.mock('./enemyDrawer.js')
 jest.mock('./collisionDetection.js')
 
-test('damageEnemy calls the enemyManager addEnemy function', () => {
-  underTest.damageEnemy(1, 2)
+test('addEnemy calls the enemyManager addEnemy function', () => {
+  underTest.addEnemy(1, 2)
   expect(enemyManager.addEnemy).toHaveBeenCalledWith(1, 2)
 })
 
-test('hitEnemy calls the enemyManager hitEnemy function', () => {
+test('damageEnemy calls the enemyManager hitEnemy function', () => {
   let obj1 = {mockProperty: 1}
   let obj2 = {otherMockProperty: 'a'}
-  underTest.hitEnemy(obj1, obj2)
+  underTest.damageEnemy(obj1, obj2)
   expect(enemyManager.hitEnemy).toHaveBeenCalledWith(obj1, obj2)
 })
