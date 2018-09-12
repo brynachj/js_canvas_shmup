b02fa4c2c61a79ec19908fc27b75afb4f7d22eeb
Brynach Jones <brynach.jones@gmail.com>
2018-09-12 20:23:39 +0100

    Removed unused sprite and updated enemyDrawer to fix all linting issues.

--- js/src/main/enemyDrawer.js
+++ js/src/main/enemyDrawer.js
@@ -1,63 +1,89 @@
-var draw_module = require('./draw.js');
+var drawModule = require('./draw.js')
 
-const UP = "up", DOWN = "down", LEFT = "left", RIGHT = "right";
+const UP = 'up'
+const DOWN = 'down'
+const LEFT = 'left'
+const RIGHT = 'right'
 
-enemy_sprite = new Image();
-enemy_sprite.src = 'images/enemy_sprite.png';
+/* global Image */
 
-enemy_sprite_left = new Image();
-enemy_sprite_left.src = 'images/enemy_sprite_left.png';
-enemy_sprite_right = new Image();
-enemy_sprite_right.src = 'images/enemy_sprite_right.png';
-enemy_sprite_up = new Image();
-enemy_sprite_up.src = 'images/enemy_sprite_up.png';
-enemy_sprite_down = new Image();
-enemy_sprite_down.src = 'images/enemy_sprite_down.png';
+let enemySpriteLeft = new Image()
+enemySpriteLeft.src = 'images/enemy_sprite_left.png'
+let enemySpriteRight = new Image()
+enemySpriteRight.src = 'images/enemy_sprite_right.png'
+let enemySpriteUp = new Image()
+enemySpriteUp.src = 'images/enemy_sprite_up.png'
+let enemySpriteDown = new Image()
+enemySpriteDown.src = 'images/enemy_sprite_down.png'
 
-attack_sprite_left = new Image();
-attack_sprite_left.src = 'images/enemy_sword_left.png';
-attack_sprite_right = new Image();
-attack_sprite_right.src = 'images/enemy_sword_right.png';
-attack_sprite_up = new Image();
-attack_sprite_up.src = 'images/enemy_sword_up.png';
-attack_sprite_down = new Image();
-attack_sprite_down.src = 'images/enemy_sword_down.png';
+let attackSpriteLeft = new Image()
+attackSpriteLeft.src = 'images/enemy_sword_left.png'
+let attackSpriteRight = new Image()
+attackSpriteRight.src = 'images/enemy_sword_right.png'
+let attackSpriteUp = new Image()
+attackSpriteUp.src = 'images/enemy_sword_up.png'
+let attackSpriteDown = new Image()
+attackSpriteDown.src = 'images/enemy_sword_down.png'
 
-function drawIdle(enemy) {
-  if(enemy.facing === LEFT){draw_module.drawSprite(enemy_sprite_left, enemy, draw_module.ctx);}
-  if(enemy.facing === RIGHT){draw_module.drawSprite(enemy_sprite_right, enemy, draw_module.ctx);}
-  if(enemy.facing === UP){draw_module.drawSprite(enemy_sprite_up, enemy, draw_module.ctx);}
-  if(enemy.facing === DOWN){draw_module.drawSprite(enemy_sprite_down, enemy, draw_module.ctx);}
+function drawIdle (enemy) {
+  if (enemy.facing === LEFT) {
+    drawModule.drawSprite(enemySpriteLeft, enemy, drawModule.ctx)
+  }
+  if (enemy.facing === RIGHT) {
+    drawModule.drawSprite(enemySpriteRight, enemy, drawModule.ctx)
+  }
+  if (enemy.facing === UP) {
+    drawModule.drawSprite(enemySpriteUp, enemy, drawModule.ctx)
+  }
+  if (enemy.facing === DOWN) {
+    drawModule.drawSprite(enemySpriteDown, enemy, drawModule.ctx)
+  }
   if (window.drawHitboxes) {
-      draw_module.drawHitbox(enemy.player_detection_box, draw_module.ctx);
-      draw_module.drawHitbox(enemy.player_aggro_box, draw_module.ctx);
-      draw_module.drawHitbox(enemy.player_attack_box, draw_module.ctx);
+    drawModule.drawHitbox(enemy.player_detection_box, drawModule.ctx)
+    drawModule.drawHitbox(enemy.player_aggro_box, drawModule.ctx)
+    drawModule.drawHitbox(enemy.player_attack_box, drawModule.ctx)
   }
 }
 
-function drawWindUpAttack(enemy) {
-  drawIdle(enemy);
-  if(enemy.facing === LEFT){draw_module.drawSprite(attack_sprite_left, {x: enemy.x, y: enemy.y+enemy.h/2}, draw_module.ctx);}
-  if(enemy.facing === RIGHT){draw_module.drawSprite(attack_sprite_right, {x: enemy.x, y: enemy.y+enemy.h/2}, draw_module.ctx);}
-  if(enemy.facing === UP){draw_module.drawSprite(attack_sprite_up, {x: enemy.x + enemy.w/2, y: enemy.y}, draw_module.ctx);}
-  if(enemy.facing === DOWN){draw_module.drawSprite(attack_sprite_down, {x: enemy.x + enemy.w/2, y: enemy.y}, draw_module.ctx);}
+function drawWindUpAttack (enemy) {
+  drawIdle(enemy)
+  if (enemy.facing === LEFT) {
+    drawModule.drawSprite(attackSpriteLeft, {x: enemy.x, y: enemy.y + enemy.h / 2}, drawModule.ctx)
+  }
+  if (enemy.facing === RIGHT) {
+    drawModule.drawSprite(attackSpriteRight, {x: enemy.x, y: enemy.y + enemy.h / 2}, drawModule.ctx)
+  }
+  if (enemy.facing === UP) {
+    drawModule.drawSprite(attackSpriteUp, {x: enemy.x + enemy.w / 2, y: enemy.y}, drawModule.ctx)
+  }
+  if (enemy.facing === DOWN) {
+    drawModule.drawSprite(attackSpriteDown, {x: enemy.x + enemy.w / 2, y: enemy.y}, drawModule.ctx)
+  }
 }
 
-function drawAttacking(enemy) {
-  drawIdle(enemy);
-  if(enemy.facing === LEFT){draw_module.drawSprite(attack_sprite_left, {x: enemy.x-30, y: enemy.y+enemy.h/2}, draw_module.ctx);}
-  if(enemy.facing === RIGHT){draw_module.drawSprite(attack_sprite_right, {x: enemy.x+30, y: enemy.y+enemy.h/2}, draw_module.ctx);}
-  if(enemy.facing === UP){draw_module.drawSprite(attack_sprite_up, {x: enemy.x + enemy.w/2, y: enemy.y-30}, draw_module.ctx);}
-  if(enemy.facing === DOWN){draw_module.drawSprite(attack_sprite_down, {x: enemy.x + enemy.w/2, y: enemy.y+30}, draw_module.ctx);}
+function drawAttacking (enemy) {
+  drawIdle(enemy)
+  if (enemy.facing === LEFT) {
+    drawModule.drawSprite(attackSpriteLeft, {x: enemy.x - 30, y: enemy.y + enemy.h / 2}, drawModule.ctx)
+  }
+  if (enemy.facing === RIGHT) {
+    drawModule.drawSprite(attackSpriteRight, {x: enemy.x + 30, y: enemy.y + enemy.h / 2}, drawModule.ctx)
+  }
+  if (enemy.facing === UP) {
+    drawModule.drawSprite(attackSpriteUp, {x: enemy.x + enemy.w / 2, y: enemy.y - 30}, drawModule.ctx)
+  }
+  if (enemy.facing === DOWN) {
+    drawModule.drawSprite(attackSpriteDown, {x: enemy.x + enemy.w / 2, y: enemy.y + 30}, drawModule.ctx)
+  }
 }
 
-function drawWindDownAttack(enemy) {
-  drawIdle(enemy);
+function drawWindDownAttack (enemy) {
+  drawIdle(enemy)
 }
 
 module.exports = {
-  drawIdle : drawIdle,
-  drawWindUpAttack : drawWindUpAttack,
-  drawAttacking : drawAttacking,
-  drawWindDownAttack : drawWindDownAttack
+  drawIdle,
+  drawWindUpAttack,
+  drawAttacking,
+  drawWindDownAttack
 }
