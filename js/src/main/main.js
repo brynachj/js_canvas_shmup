f0403fc676886e05fdbacb95e963894386726be1
Brynach Jones <brynach.jones@gmail.com>
2018-09-16 20:35:54 +0100

    Fix all remaining linting issues in main.js

--- js/src/main/main.js
+++ js/src/main/main.js
@@ -1,11 +1,11 @@
-var debug_module = require('./debugControls.js');
-var player_module = require('./player.js');
-var enemy_service = require('./enemy/enemyService.js');
-var draw_module = require('./draw.js');
-var pebble_module = require('./pebble.js');
-var pebble_pickup_module = require('./pebblePickup.js');
-var hud_module = require('./hud.js');
-var collision_detection_module = require('./collisionDetection.js');
+var debugModule = require('./debugControls.js')
+var playerModule = require('./player.js')
+var enemyService = require('./enemy/enemyService.js')
+var drawModule = require('./draw.js')
+var pebbleModule = require('./pebble.js')
+var pebblePickupModule = require('./pebblePickup.js')
+var hudModule = require('./hud.js')
+var collisionDetectionModule = require('./collisionDetection.js')
 var eventListener = require('./eventListener.js')
 
 const CANVAS = 'canvas'
@@ -15,69 +15,69 @@ var width = 600
 var height = 600
 
 function enemyHitTest () { // should be in enemy classes
-  pebble_module.pebbles.map(pebble => {
-    enemy_service.getEnemies().filter(enemy => collision_detection_module.collisionDetection(pebble, enemy)).map(enemy => {
-      enemy_service.damageEnemy(enemy, 15)
-      pebble_module.removeFromPebbles(pebble)
-      player_module.addExperience(10) // Should be tied to damaging the enemy - 1/5 of damage done
+  pebbleModule.pebbles.map(pebble => {
+    enemyService.getEnemies().filter(enemy => collisionDetectionModule.collisionDetection(pebble, enemy)).map(enemy => {
+      enemyService.damageEnemy(enemy, 15)
+      pebbleModule.removeFromPebbles(pebble)
+      playerModule.addExperience(10) // Should be tied to damaging the enemy - 1/5 of damage done
     })
   })
 }
 
-function updateEnemies() {
-  enemyHitTest();
-  enemy_service.updateEnemies();
+function updateEnemies () {
+  enemyHitTest()
+  enemyService.updateEnemies()
 }
 
-function pebblePickupCollision() {
-  pebble_pickup_module.pebblePickups.filter(p => collision_detection_module.collisionDetection(player_module.getPlayer(), p)).map(p => pickUpPebbles(p));
+function pebblePickupCollision () {
+  pebblePickupModule.pebblePickups.filter(p => collisionDetectionModule.collisionDetection(playerModule.getPlayer(), p)).map(p => pickUpPebbles(p))
 }
 
-function pickUpPebbles(pebble) {
-  pebble_module.addToAmmo(3);
-  pebble_pickup_module.removeFromPebblePickups(pebble);
+function pickUpPebbles (pebble) {
+  pebbleModule.addToAmmo(3)
+  pebblePickupModule.removeFromPebblePickups(pebble)
 }
 
-function updateText() {
+function updateText () {
   if (!eventListener.getGameStarted()) {
-    hud_module.startScreen(draw_module.ctx);
+    hudModule.startScreen(drawModule.ctx)
   }
-    hud_module.updateHud(draw_module.ctx);
-  if (!player_module.getAlive()) {
-    hud_module.deathScreen(draw_module.ctx);
+  hudModule.updateHud(drawModule.ctx)
+  if (!playerModule.getAlive()) {
+    hudModule.deathScreen(drawModule.ctx)
   }
 }
 
 // Initialisations
-enemy_service.addEnemy(Math.random() * 600, Math.random() * 600);
+enemyService.addEnemy(Math.random() * 600, Math.random() * 600)
 
-function clearCanvas() {
-  draw_module.ctx.clearRect(0,0,width,height);
+function clearCanvas () {
+  drawModule.ctx.clearRect(0, 0, width, height)
 }
 
-function init() {
-  canvas = document.getElementById(CANVAS);
-  draw_module.ctx = canvas.getContext('2d');
+function init () {
+  canvas = document.getElementById(CANVAS)
+  drawModule.ctx = canvas.getContext('2d')
   eventListener.initialiseEventListeners()
-  if(debug_module.debug) {
-    debug_module.addDebugControls();
-    debug_module.addCheckBoxEventListeners();
+  if (debugModule.debug) {
+    debugModule.addDebugControls()
+    debugModule.addCheckBoxEventListeners()
   }
-  gameLoop();
+  gameLoop()
 }
 
-function gameLoop() {
-  clearCanvas();
-  if(player_module.getAlive() && eventListener.getGameStarted()){
-    updateEnemies();
-    pebblePickupCollision();
+function gameLoop () {
+  clearCanvas()
+  if (playerModule.getAlive() && eventListener.getGameStarted()) {
+    updateEnemies()
+    pebblePickupCollision()
     eventListener.updateGameWorld()
-    pebble_module.moveOnScreenPebbles();
-    pebble_pickup_module.drawPebblePickup(draw_module.ctx);
-    pebble_module.drawOnScreenPebble(draw_module.ctx);
+    pebbleModule.moveOnScreenPebbles()
+    pebblePickupModule.drawPebblePickup(drawModule.ctx)
+    pebbleModule.drawOnScreenPebble(drawModule.ctx)
   }
-  updateText();
-  game = setTimeout(gameLoop, 1000 / 40);
+  updateText()
+  setTimeout(gameLoop, 1000 / 40)
 }
 
-window.onload = init;
+window.onload = init
