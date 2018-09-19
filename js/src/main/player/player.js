64f6eee83e33428383f668d866c420b25d7fcf58
Brynach Jones <brynach.jones@gmail.com>
2018-09-19 07:44:28 +0100

    Fix linting in player.js

--- js/src/main/player/player.js
+++ js/src/main/player/player.js
@@ -1,79 +1,89 @@
-var player_drawer = require('./playerDrawer.js');
-var player_movement = require('./playerMovement.js');
-var player_attack = require('./playerAttack.js');
+var playerDrawer = require('./playerDrawer.js')
+var playerMovement = require('./playerMovement.js')
+var playerAttack = require('./playerAttack.js')
 
-const WIDTH = 20, HEIGHT = 26, SPEED = 10, ATTACK_WIDTH = 15;
+const WIDTH = 20
+const HEIGHT = 26
+const ATTACK_WIDTH = 15
 
-var player, health, alive = true, experience = 0;
+let player
+let health
+let alive = true
+let experience = 0
 
-hitEnemyList = [];
-
-// player directions
-const UP = 'up', DOWN = 'down', LEFT = 'left', RIGHT = 'right';
+// player direction
+const RIGHT = 'right'
 
 // player states
-const IDLE = 'idle', ATTACKING = 'attacking', WINDING_DOWN = 'winding_down';
+const IDLE = 'idle'
+const ATTACKING = 'attacking'
+const WINDING_DOWN = 'winding_down'
 
-function createPlayer(x1, y1) {
-  return {x : x1, y : y1, w : WIDTH, h : HEIGHT, hitBoxColor : '#7cfc00',
-  state: IDLE, attackAnimationFrame : 0, attack_box: {x:x1+WIDTH, y:y1-5, w:ATTACK_WIDTH, h:HEIGHT+10, hitBoxColor: '#ff6961'},
-  facing: RIGHT};
+function createPlayer (x1, y1) {
+  return {x: x1,
+    y: y1,
+    w: WIDTH,
+    h: HEIGHT,
+    hitBoxColor: '#7cfc00',
+    state: IDLE,
+    attackAnimationFrame: 0,
+    attack_box: {x: x1 + WIDTH, y: y1 - 5, w: ATTACK_WIDTH, h: HEIGHT + 10, hitBoxColor: '#ff6961'},
+    facing: RIGHT}
 }
 
-function getPlayer(){
-  if(player === undefined) {
-    resetPlayer();
+function getPlayer () {
+  if (player === undefined) {
+    resetPlayer()
   }
-  return player;
+  return player
 }
 
-function resetPlayer() {
-  experience = 0;
-  health = 100;
-  player = createPlayer(10, 287);
+function resetPlayer () {
+  experience = 0
+  health = 100
+  player = createPlayer(10, 287)
 }
 
-function getHealth() {
-  return health;
+function getHealth () {
+  return health
 }
 
-function getAlive() {
-  return alive;
+function getAlive () {
+  return alive
 }
 
-function setAlive(isAlive) {
-  return alive = isAlive;
+function setAlive (isAlive) {
+  alive = isAlive
 }
 
-function getExperience() {
-  return experience;
+function getExperience () {
+  return experience
 }
 
-function addExperience(value) {
-  experience += value;
+function addExperience (value) {
+  experience += value
 }
 
-function updateHealth(value) {
-  health += value;
+function updateHealth (value) {
+  health += value
   if (health > 0) {
     // TODO: iframes and such
   } else {
-    alive = false;
+    alive = false
   }
 }
 
-function updatePlayer(rightKey, leftKey, upKey, downKey) {
-  if(player.state === ATTACKING || player.state === WINDING_DOWN){
-    console.log("fighting");
-    player_attack.attack(player);
+function updatePlayer (rightKey, leftKey, upKey, downKey) {
+  if (player.state === ATTACKING || player.state === WINDING_DOWN) {
+    playerAttack.attack(player)
   }
-  player_movement.movePlayer(player, rightKey, leftKey, upKey, downKey);
-  player_movement.facePlayer(player, rightKey, leftKey, upKey, downKey);
-  player_drawer.drawPlayer(player);
+  playerMovement.movePlayer(player, rightKey, leftKey, upKey, downKey)
+  playerMovement.facePlayer(player, rightKey, leftKey, upKey, downKey)
+  playerDrawer.drawPlayer(player)
 }
 
-function attack() {
-  player.state = ATTACKING;
+function attack () {
+  player.state = ATTACKING
 }
 
 function dash () {
