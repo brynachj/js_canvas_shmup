31d1722fac0a7c0e9dbc77c055c5adb7e771dc97
Brynach Jones <brynach.jones@gmail.com>
2018-10-17 19:17:02 +0100

    Fix linting issues in the pebble.js class

--- js/src/main/pebble.js
+++ js/src/main/pebble.js
@@ -1,71 +1,73 @@
-var draw_module = require('./draw.js');
-var utility_module = require('./utility.js');
+var drawModule = require('./draw.js')
+var utilityModule = require('./utility.js')
 
-const WIDTH = 4, HEIGHT = 5, SPEED = 10;
+const WIDTH = 4
+const HEIGHT = 5
 
-pebble_sprite = new Image();
-pebble_sprite.src = 'images/pebble.png';
+/* global Image */
 
-var pebbles = [];
-var ammo = 10;
+let pebbleSprite = new Image()
+pebbleSprite.src = 'images/pebble.png'
 
-function getAmmo() {
-  return ammo;
+var pebbles = []
+var ammo = 10
+
+function getAmmo () {
+  return ammo
 }
 
-function resetPebbleAmmo() {
-  ammo = 10;
+function resetPebbleAmmo () {
+  ammo = 10
 }
 
-function takeOneFromAmmo() {
-  ammo--;
+function takeOneFromAmmo () {
+  ammo--
 }
 
-function addToAmmo(i) {
-  ammo += i;
+function addToAmmo (i) {
+  ammo += i
 }
 
-function addToPebbles(x, y) {
-  pebbles.push(createPebble(x,y));
+function addToPebbles (x, y) {
+  pebbles.push(createPebble(x, y))
 }
 
-function removeFromPebbles(pebble) {
-  var index = pebbles.map(p => p.id).indexOf(pebble.id);
-  pebbles.splice(index,1);
+function removeFromPebbles (pebble) {
+  var index = pebbles.map(p => p.id).indexOf(pebble.id)
+  pebbles.splice(index, 1)
 }
 
-function createPebble(x1, y1) {
-  return {id: utility_module.newId(pebbles), x:x1, y:y1, w:WIDTH, h:HEIGHT, hitBoxColor: '#00bfff'};
+function createPebble (x1, y1) {
+  return {id: utilityModule.newId(pebbles), x: x1, y: y1, w: WIDTH, h: HEIGHT, hitBoxColor: '#00bfff'}
 }
 
-function moveOnScreenPebbles() {
-  pebbles.map(pebble => movePebble(pebble));
+function moveOnScreenPebbles () {
+  pebbles.map(pebble => movePebble(pebble))
 }
 
-function movePebble(pebble) {
-  if (pebble.x < 600 + pebble_sprite.width) {
-    pebble.x += 10;
+function movePebble (pebble) {
+  if (pebble.x < 600 + pebbleSprite.width) {
+    pebble.x += 10
   } else {
-    removeFromPebbles(pebble);
+    removeFromPebbles(pebble)
   }
 }
 
-function drawOnScreenPebble(ctx) {
-  if (pebbles.length){
-    pebbles.map(pebble => draw_module.drawSprite(pebble_sprite, pebble, ctx));
+function drawOnScreenPebble (ctx) {
+  if (pebbles.length) {
+    pebbles.map(pebble => drawModule.drawSprite(pebbleSprite, pebble, ctx))
   }
 }
 
 module.exports = {
-  pebbles : pebbles,
-  createPebble : createPebble,
-  moveOnScreenPebbles : moveOnScreenPebbles,
-  drawOnScreenPebble : drawOnScreenPebble,
-  addToPebbles : addToPebbles,
-  removeFromPebbles : removeFromPebbles,
-
-  resetPebbleAmmo : resetPebbleAmmo,
-  takeOneFromAmmo : takeOneFromAmmo,
-  addToAmmo : addToAmmo,
-  getAmmo : getAmmo
+  pebbles,
+  createPebble,
+  moveOnScreenPebbles,
+  drawOnScreenPebble,
+  addToPebbles,
+  removeFromPebbles,
+  resetPebbleAmmo,
+  takeOneFromAmmo,
+  addToAmmo,
+  getAmmo
 }
