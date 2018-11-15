54da8424df9bf5c3210d80007c013f23db8f70c7
Brynach Jones <brynach.jones@gmail.com>
2018-11-15 08:23:20 +0000

    Add another collisionDetection test

--- js/src/test/collisionDetection.test.js
+++ js/src/test/collisionDetection.test.js
@@ -11,3 +11,15 @@ test('collisionDetection returns true when two objects are in the same place', (
   let secondObject = {x: 0, y: 0, w: 10, h: 10}
   expect(underTest.collisionDetection(firstObject, secondObject)).toBe(true)
 })
+
+test('collisionDetection returns true when an object is inside another object', () => {
+  let firstObject = {x: 0, y: 0, w: 10, h: 10}
+  let secondObject = {x: 2, y: 2, w: 5, h: 5}
+  expect(underTest.collisionDetection(firstObject, secondObject)).toBe(true)
+})
+
+test('collisionDetection returns false when two objects dont collide', () => {
+  let firstObject = {x: 0, y: 0, w: 10, h: 10}
+  let secondObject = {x: 20, y: 20, w: 10, h: 10}
+  expect(underTest.collisionDetection(firstObject, secondObject)).toBe(false)
+})
