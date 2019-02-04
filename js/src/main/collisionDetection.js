26a848d116d74ec793de71d634262c37d1d98670
Brynach Jones <brynach.jones@gmail.com>
2019-02-04 20:35:14 +0000

    Revert "Fix linting in collisionDetection module."
    This reverts commit 7f582855f8e8772f97f353617c633d9ac30ad3d1.

--- js/src/main/collisionDetection.js
+++ js/src/main/collisionDetection.js
@@ -1,6 +1,6 @@
-function collisionDetection (firstThing, secondThing) {
+function collisionDetection(firstThing, secondThing){
   return firstThing.x < secondThing.x + secondThing.w && firstThing.x + firstThing.w > secondThing.x &&
-  firstThing.y < secondThing.y + secondThing.h && firstThing.h + firstThing.y > secondThing.y
+  firstThing.y < secondThing.y + secondThing.h && firstThing.h + firstThing.y > secondThing.y;
 }
 
 function isObjectBetweenTwoObjects (objectBetween, firstObject, secondObject) {
