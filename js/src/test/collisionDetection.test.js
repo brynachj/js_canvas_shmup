e06832eb8e85be2f9b262e062257389ec39d298d
Brynach Jones <brynach.jones@gmail.com>
2019-02-04 20:35:13 +0000

    Revert "Added tests for collisionDetection"
    This reverts commit 52c6b8138d8bfd9b34434d025df3221d9cc76002.

--- js/src/test/collisionDetection.test.js
+++ js/src/test/collisionDetection.test.js
@@ -1,13 +1,50 @@
-const underTest = require('../main/collisionDetection.js')
+const underTest = require('../main/pebble.js')
 
-test('collisionDetection returns true when two objects are colliding', () => {
-  let firstObject = {x: 0, y: 0, w: 10, h: 10}
-  let secondObject = {x: 5, y: 5, w: 10, h: 10}
-  expect(underTest.collisionDetection(firstObject, secondObject)).toBe(true)
+afterEach(() => {
+  underTest.resetPebbleAmmo()
 })
 
-test('collisionDetection returns true when two objects are in the same place', () => {
-  let firstObject = {x: 0, y: 0, w: 10, h: 10}
-  let secondObject = {x: 0, y: 0, w: 10, h: 10}
-  expect(underTest.collisionDetection(firstObject, secondObject)).toBe(true)
+test('getAmmo by default returns 10', () => {
+  expect(underTest.getAmmo()).toBe(10)
+})
+
+test('takeOneFromAmmo reduces the number of ammo left by 1', () => {
+  expect(underTest.getAmmo()).toBe(10)
+
+  underTest.takeOneFromAmmo()
+
+  expect(underTest.getAmmo()).toBe(9)
+})
+
+test('resetPebbleAmmo resets the number of ammo left to 10', () => {
+  expect(underTest.getAmmo()).toBe(10)
+
+  underTest.takeOneFromAmmo()
+  underTest.takeOneFromAmmo()
+  expect(underTest.getAmmo()).toBe(8)
+  underTest.resetPebbleAmmo()
+
+  expect(underTest.getAmmo()).toBe(10)
+})
+
+test('addToAmmo adds the number parameterised into the function to ammo', () => {
+  expect(underTest.getAmmo()).toBe(10)
+
+  underTest.addToAmmo(12)
+
+  expect(underTest.getAmmo()).toBe(22)
+
+  underTest.addToAmmo(9)
+
+  expect(underTest.getAmmo()).toBe(31)
+})
+
+test('addToPebbles pushes a new item onto the pebbles array', () => {
+  expect(underTest.pebbles.length).toBe(0)
+
+  let character = {x: 0, y: 0, facing: 'RIGHT'}
+
+  underTest.addToPebbles(character)
+
+  expect(underTest.pebbles.length).toBe(1)
 })
