3a9dd6f5ffc273cad12561a205d42b4974b21208
Brynach Jones <brynach.jones@gmail.com>
2019-02-05 19:57:39 +0000

    Fixed all linting issues in debugControls.js

--- js/src/main/debugControls.js
+++ js/src/main/debugControls.js
@@ -1,30 +1,30 @@
 // TODO: Find a way around having drawHitboxes at window scope
-const DEBUG_DIV = 'debug-controls';
+const DEBUG_DIV = 'debug-controls'
 
-window.drawHitboxes = false;
+window.drawHitboxes = false
 
-function addDebugControls() {
-  debugDiv = document.getElementById(DEBUG_DIV);
-  debugDiv.innerHTML = '<h1>Debug</h1>';
-  debugDiv.innerHTML += '<input type="checkbox" id="hitbox_toggle">Show HitBoxes</input>';
+function addDebugControls () {
+  let debugDiv = document.getElementById(DEBUG_DIV)
+  debugDiv.innerHTML = '<h1>Debug</h1>'
+  debugDiv.innerHTML += '<input type="checkbox" id="hitbox_toggle">Show HitBoxes</input>'
 }
 
-function addCheckBoxEventListeners() {
-  var checkbox = document.getElementById('hitbox_toggle');
+function addCheckBoxEventListeners () {
+  var checkbox = document.getElementById('hitbox_toggle')
 
-  checkbox.addEventListener( 'change', function() {
-    window.drawHitboxes = this.checked;
-  });
+  checkbox.addEventListener('change', function () {
+    window.drawHitboxes = this.checked
+  })
 }
 
-function writeOutDebug(debugInfo) {
-  debugDiv = document.getElementById(DEBUG_DIV);
+function writeOutDebug (debugInfo) {
+  let debugDiv = document.getElementById(DEBUG_DIV)
   debugDiv.innerHTML += '<p>' + debugInfo + '</p>'
 }
 
 module.exports = {
-  debug : true,
-  addDebugControls : addDebugControls,
-  addCheckBoxEventListeners :addCheckBoxEventListeners,
-  writeOutDebug : writeOutDebug
+  debug: true,
+  addDebugControls,
+  addCheckBoxEventListeners,
+  writeOutDebug
 }
