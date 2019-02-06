8754d964c1a5035c5407b430bbb6651d3fd5b5d0
Brynach Jones <brynach.jones@gmail.com>
2019-02-06 19:41:34 +0000

    Created levelService with methods required for progressing to next levels

---
+++ js/src/main/levels/levelService.js
@@ -0,0 +1,37 @@
+var levelOne = require('./one.js')
+var wallService = require('../wallService.js')
+var enemyService = require('../enemy/enemyService.js')
+
+var levels = [levelOne, levelOne]
+var levelNumber = 0
+
+function initialiseNextLevel () {
+  if (levels[levelNumber].level) {
+    let rowNumber = 0
+    levelOne.level.map(row => {
+      let columnNumber = 0
+      row.map(column => {
+        if (column === 1) {
+          wallService.addWall(columnNumber * 30, rowNumber * 30)
+        } else if (column === 2) {
+          enemyService.addEnemy(columnNumber * 30, rowNumber * 30)
+        }
+        columnNumber++
+      })
+      rowNumber++
+    })
+    levelNumber++
+  } else {
+    enemyService.addEnemy(Math.random() * 600, Math.random() * 600)
+    wallService.addWall(Math.random() * 600, Math.random() * 600)
+  }
+}
+
+function getLevelNumber() {
+  return levelNumber
+}
+
+module.exports = {
+  initialiseNextLevel,
+  getLevelNumber
+}
