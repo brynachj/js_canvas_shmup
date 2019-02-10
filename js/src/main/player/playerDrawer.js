778bfd3bbae94da1e671ba13a1505d85a6b624c0
Brynach Jones <brynach.jones@gmail.com>
2019-02-10 20:23:21 +0000

    Add new sprite for all directions when player attacks

--- js/src/main/player/playerDrawer.js
+++ js/src/main/player/playerDrawer.js
@@ -6,21 +6,23 @@ var constants = require('../shared/constants.js')
 let playerSprite = new Image()
 playerSprite.src = 'images/player.png'
 
-let attackSprite = new Image()
-attackSprite.src = 'images/player_attack.png'
+let attackSpriteHorizontal = new Image()
+attackSpriteHorizontal.src = 'images/player_attack_horizontal.png'
+let attackSpriteVertical = new Image()
+attackSpriteVertical.src = 'images/player_attack_vertical.png'
 
 function drawAttacking (player) {
-  if (player.facing === constants.RIGHT) { drawModule.drawSprite(attackSprite, { x: player.x + 15, y: player.y - 5 }, drawModule.ctx) }
-  if (player.facing === constants.LEFT) { drawModule.drawSprite(attackSprite, { x: player.x - 15, y: player.y - 5 }, drawModule.ctx) }
-  if (player.facing === constants.UP) { drawModule.drawSprite(attackSprite, { x: player.x, y: player.y - 15 }, drawModule.ctx) }
-  if (player.facing === constants.DOWN) { drawModule.drawSprite(attackSprite, { x: player.x, y: player.y + 5 }, drawModule.ctx) }
+  if (player.facing === constants.RIGHT) { drawModule.drawSprite(attackSpriteHorizontal, { x: player.x + 15, y: player.y - 5 }, drawModule.ctx) }
+  if (player.facing === constants.LEFT) { drawModule.drawSprite(attackSpriteHorizontal, { x: player.x - 15, y: player.y - 5 }, drawModule.ctx) }
+  if (player.facing === constants.UP) { drawModule.drawSprite(attackSpriteVertical, { x: player.x - 10, y: player.y - 15 }, drawModule.ctx) }
+  if (player.facing === constants.DOWN) { drawModule.drawSprite(attackSpriteVertical, { x: player.x - 10, y: player.y + 25 }, drawModule.ctx) }
 }
 
 function drawWindUpAttack (player) {
-  if (player.facing === constants.RIGHT) { drawModule.drawSprite(attackSprite, { x: player.x, y: player.y - 5 }, drawModule.ctx) }
-  if (player.facing === constants.LEFT) { drawModule.drawSprite(attackSprite, { x: player.x, y: player.y - 5 }, drawModule.ctx) }
-  if (player.facing === constants.UP) { drawModule.drawSprite(attackSprite, { x: player.x, y: player.y - 5 }, drawModule.ctx) }
-  if (player.facing === constants.DOWN) { drawModule.drawSprite(attackSprite, { x: player.x, y: player.y - 5 }, drawModule.ctx) }
+  if (player.facing === constants.RIGHT) { drawModule.drawSprite(attackSpriteHorizontal, { x: player.x, y: player.y - 5 }, drawModule.ctx) }
+  if (player.facing === constants.LEFT) { drawModule.drawSprite(attackSpriteHorizontal, { x: player.x, y: player.y - 5 }, drawModule.ctx) }
+  if (player.facing === constants.UP) { drawModule.drawSprite(attackSpriteHorizontal, { x: player.x, y: player.y - 5 }, drawModule.ctx) }
+  if (player.facing === constants.DOWN) { drawModule.drawSprite(attackSpriteHorizontal, { x: player.x, y: player.y - 5 }, drawModule.ctx) }
 }
 
 function drawPlayer (player) {
